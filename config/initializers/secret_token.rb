cc504f4457f1b13b379abf101fe46c9728b62e11
Tim Habermaas <tim.habermaas@gmail.com>
2011-11-23 22:24:49 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WCACompetitions::Application.config.secret_token = 'ab605cff4d34b2c425616be8349f989463420dc91335b43d7fb92ac592df4f434aff372fef11acd79810781ef3ea752bc031e893ae0bf041d33251aa5324ee26'
