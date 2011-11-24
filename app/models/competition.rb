bb390958e90d3a1468b0b354130e69c5dba96fdc
Tim Habermaas <tim.habermaas@gmail.com>
2011-11-24 22:48:25 +0100

    competitions usually start before they end

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -3,4 +3,12 @@ class Competition < ActiveRecord::Base
 
   validates :name, :starts_at, :ends_at, :presence => true
   validates :name, :uniqueness => true
+  validate :cannot_end_before_it_started
+
+  private
+  def cannot_end_before_it_started
+    unless starts_at.nil? or ends_at.nil?
+      errors.add(:ends_at, "can't be prior to start date") if ends_at < starts_at
+    end
+  end
 end
--- spec/models/competition_spec.rb
+++ spec/models/competition_spec.rb
@@ -12,5 +12,15 @@ describe Competition do
       c2.should_not be_valid
       c2.errors[:name].should_not be_empty
     end
+
+    it "starts before it ends" do
+      competition = build(:competition, :starts_at => Date.new(2011, 2, 13),
+                                        :ends_at => Date.new(2011, 2, 11))
+      competition.should_not be_valid
+      competition.errors[:ends_at].should_not be_empty
+      competition = build(:competition, :starts_at => Date.new(2011, 2, 13),
+                                        :ends_at => Date.new(2011, 2, 13))
+      competition.should be_valid
+    end
   end
 end
