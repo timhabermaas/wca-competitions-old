9ea58f374e300378962106d665c4c682e90f7efc
Tim Habermaas <tim.habermaas@gmail.com>
2011-11-30 04:01:11 +0100

    fetch existing competitor if wca_id is present

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,6 +11,11 @@ class RegistrationsController < ApplicationController
 
   def create
     @registration = @competition.registrations.build params[:registration]
+    unless params[:registration][:competitor_attributes][:wca_id].blank?
+      competitor = Competitor.find_by_wca_id params[:registration][:competitor_attributes][:wca_id]
+      @registration.competitor = competitor unless competitor.nil?
+    end
+
     if @registration.save
       redirect_to competition_registrations_path(@competition), :notice => "Successfully registered"
     else
--- spec/requests/registrations_spec.rb
+++ spec/requests/registrations_spec.rb
@@ -5,13 +5,15 @@ require "spec_helper"
 describe "Registrations" do
   before :each do
     @competition = create :competition, :name => "Munich Open 2011"
-    @dieter = create :competitor, :first_name => "Dieter", :last_name => "Müller", :wca_id => "2008MULL01"
-    @peter = create :competitor, :first_name => "Peter", :last_name => "Müller"
-    @registraion = Registration.create :competition => @competition, :competitor => @dieter, :email => "muh@cow.com"
-    @registraion = Registration.create :competition => @competition, :competitor => @peter, :email => "muh2@cow.com"
   end
-
   describe "GET /registrations" do
+    before :each do
+      @dieter = create :competitor, :first_name => "Dieter", :last_name => "Müller", :wca_id => "2008MULL01"
+      @peter = create :competitor, :first_name => "Peter", :last_name => "Müller"
+      @registraion = Registration.create :competition => @competition, :competitor => @dieter, :email => "muh@cow.com"
+      @registraion = Registration.create :competition => @competition, :competitor => @peter, :email => "muh2@cow.com"
+    end
+
     it "lists all registered competitors for Munich Open" do
       visit competition_registrations_path(@competition)
       page.should have_link("Dieter Müller", :href => "http://worldcubeassociation.org/results/p.php?i=2008MULL01")
@@ -31,6 +33,17 @@ describe "Registrations" do
       page.should have_content("Peter Mustermann")
     end
 
-    it "doesn't create a new competitor if his WCA ID already exists"
+    it "doesn't create a new competitor if his WCA ID already exists" do
+      create :competitor, :first_name => "Dieter", :last_name => "Müller", :wca_id => "2008MULL01"
+      visit new_competition_registration_path(@competition)
+      fill_in "First name", :with => "Dieter"
+      fill_in "Last name", :with => "Müller"
+      fill_in "Email", :with => "peter@mustermann.de"
+      fill_in "WCA ID", :with => "2008MULL01"
+      click_on "Register"
+      page.should have_content("Successfully registered")
+      page.should have_content("Dieter Müller")
+      Competitor.count.should == 1
+    end
   end
 end
