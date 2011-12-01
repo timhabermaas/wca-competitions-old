05561bd3855f136f5faf73d4fc14807b63afd459
Tim Habermaas <tim.habermaas@gmail.com>
2011-12-01 22:16:14 +0100

    rename method

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -8,14 +8,14 @@ class Competition < ActiveRecord::Base
 
   validates :name, :starts_at, :ends_at, :user_id, :presence => true
   validates :name, :uniqueness => true
-  validate :cannot_end_before_it_started
+  validate :starts_before_it_ends
 
   def days
     (ends_at - starts_at).to_i + 1
   end
 
   private
-  def cannot_end_before_it_started
+  def starts_before_it_ends
     unless starts_at.nil? or ends_at.nil?
       errors.add(:ends_at, "can't be prior to start date") if ends_at < starts_at
     end
