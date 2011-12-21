70bb5138fe7942597b0be996785ef7a2487f9551
Tim Habermaas <tim.habermaas@gmail.com>
2011-12-21 04:25:20 +0100

    make locale parameter in url prettier

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 WCACompetitions::Application.routes.draw do
-  get "/log_in" => "sessions#new", :as => "log_in"
-  get "/log_out" => "sessions#destroy", :as => "log_out"
-  resource :session
+  scope "/(:locale)"  do
+    get "/log_in" => "sessions#new", :as => "log_in"
+    get "/log_out" => "sessions#destroy", :as => "log_out"
+    resource :session
 
-  resources :competitions do
-    resources :news
-    resources :registrations
-    resources :schedules
+    resources :competitions do
+      resources :news
+      resources :registrations
+      resources :schedules
+    end
+    resources :events
   end
-  resources :events
 
+  match "/:locale" => "competitions#index"
   root :to => "competitions#index"
 end
