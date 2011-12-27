f493b83da3bd5b16af8dfcc6c8e91ecaeb93d873
Tim Habermaas <tim.habermaas@gmail.com>
2011-12-27 03:40:41 +0100

    configure url for wca api in config file

--- app/models/wca/base.rb
+++ app/models/wca/base.rb
@@ -1,3 +1,3 @@
 class WCA::Base < ActiveResource::Base
-  self.site = "http://localhost:9292"
+  self.site = Rails.application.config.wca_api_url
 end
--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,7 @@ module WCACompetitions
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.wca_api_url = "http://localhost:9292"
   end
 end
