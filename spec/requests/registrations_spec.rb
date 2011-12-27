f4e3fc85289264543cb17749842b4d494a2e4908
Tim Habermaas <tim.habermaas@gmail.com>
2011-12-27 01:39:03 +0100

    add fastest single to compare page

--- spec/requests/registrations_spec.rb
+++ spec/requests/registrations_spec.rb
@@ -134,10 +134,13 @@ describe "Registrations" do
         within("#compare tbody") do
           find(:xpath, ".//tr[1]").text.should match("Basti")
           find(:xpath, ".//tr[1]").text.should match("13.50")
+          find(:xpath, ".//tr[1]").text.should match("10.46")
           find(:xpath, ".//tr[2]").text.should match("Stefan")
           find(:xpath, ".//tr[2]").text.should match("14.66")
+          find(:xpath, ".//tr[2]").text.should match("9.56")
           find(:xpath, ".//tr[3]").text.should match("Tim")
           find(:xpath, ".//tr[3]").text.should match("15.79")
+          find(:xpath, ".//tr[3]").text.should match("12.52")
           page.should_not have_content("Ron")
           page.should_not have_content("Thomas")
         end
