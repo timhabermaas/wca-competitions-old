d6546719d9e4ac17d4b16bab0c922a8253941b1d
Tim Habermaas <tim.habermaas@gmail.com>
2011-12-29 19:29:21 +0100

    remove pending specs for localizing results

--- spec/helpers/registrations_helper_spec.rb
+++ spec/helpers/registrations_helper_spec.rb
@@ -2,40 +2,24 @@ require "spec_helper"
 
 describe RegistrationsHelper do
   describe "#format_time" do
-    context "english locale" do
-      before :each do
-        I18n.locale = :en
-      end
-
-      it "formats 1324 as 13.24" do
-        helper.format_time(1324).should == "13.24"
-      end
-
-      it "formats 1304 as 13.04" do
-        helper.format_time(1304).should == "13.04"
-      end
-
-      it "formats 6000 as 1:00.00" do
-        helper.format_time(6000).should == "1:00.00"
-      end
-
-      it "formats 360000 as 1:00:00.00" do
-        helper.format_time(360000).should == "1:00:00.00"
-      end
+    before :each do
+      I18n.locale = :en
     end
 
-    context "german locale" do
-      before :each do
-        I18n.locale = :de
-      end
+    it "formats 1324 as 13.24" do
+      helper.format_time(1324).should == "13.24"
+    end
 
-      xit "formats 1324 as 13.24" do
-        helper.format_time(1324).should == "13,24"
-      end
+    it "formats 1304 as 13.04" do
+      helper.format_time(1304).should == "13.04"
+    end
 
-      xit "formats 1304 as 13.04" do
-        helper.format_time(1304).should == "13,04"
-      end
+    it "formats 6000 as 1:00.00" do
+      helper.format_time(6000).should == "1:00.00"
+    end
+
+    it "formats 360000 as 1:00:00.00" do
+      helper.format_time(360000).should == "1:00:00.00"
     end
   end
 
