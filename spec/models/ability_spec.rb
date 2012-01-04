97305e6b86077b6c36e3047623efd928e324e2f7
Tim Habermaas <tim.habermaas@gmail.com>
2012-01-04 01:11:19 +0100

    clean up specs #2

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -5,91 +5,76 @@ describe Ability do
   let(:organizer) { create :organizer }
 
   context "not logged in" do
-    before :each do
-      @ability = Ability.new(nil)
-    end
+    subject { Ability.new(nil) }
 
     context "open competition" do
+      let(:competition) { create :competition }
+
       it "should be able to register for open competitions" do
-        competition = create :competition
-        @ability.should be_able_to(:new, Registration.new(:competition => competition))
-        @ability.should be_able_to(:create, Registration.new(:competition => competition))
+        should be_able_to(:new, Registration.new(:competition => competition))
+        should be_able_to(:create, Registration.new(:competition => competition))
       end
     end
 
     context "closed competition" do
-      before :each do
-        @competition = create :competition, :closed => true
-      end
+      let(:competition) { create :competition, :closed => true }
 
       it "should see registrations" do
-        @ability.should be_able_to(:index, Registration.new(:competition => @competition))
+        should be_able_to(:index, Registration.new(:competition => competition))
       end
 
       it "should not be able to register" do
-        @ability.should_not be_able_to(:new, Registration.new(:competition => @competition))
-        @ability.should_not be_able_to(:create, Registration.new(:competition => @competition))
+        should_not be_able_to(:new, Registration.new(:competition => competition))
+        should_not be_able_to(:create, Registration.new(:competition => competition))
       end
-    end
-  end
-
-  context "logged in as admin" do
-    before :each do
-      @ability = Ability.new(admin)
-    end
-
-    it "can update news" do
-      @ability.should be_able_to(:update, News.new)
     end
   end
 
   context "logged in as organizer" do
-    before :each do
-      @ability = Ability.new(organizer)
-      @competition = create :competition, :user => organizer
-    end
+    subject { Ability.new(organizer) }
+    let(:competition) { create :competition, :user => organizer }
 
     describe "news" do
       it "can only create news to competitions he created" do
-        @ability.should be_able_to(:create, @competition.news.build)
-        @ability.should_not be_able_to(:create, News.new)
+        should be_able_to(:create, competition.news.build)
+        should_not be_able_to(:create, News.new)
       end
 
       it "can only update those he created" do
-        news = create(:news, :competition => @competition, :user => organizer)
-        @ability.should be_able_to(:update, news)
-        @ability.should_not be_able_to(:update, News.new)
+        news = create(:news, :competition => competition, :user => organizer)
+        should be_able_to(:update, news)
+        should_not be_able_to(:update, News.new)
       end
     end
 
     describe "registrations" do
       it "can update registrations for his own competitions" do
-        @ability.should be_able_to(:update, @competition.registrations.build)
-        @ability.should_not be_able_to(:update, Registration.new)
+        should be_able_to(:update, competition.registrations.build)
+        should_not be_able_to(:update, Registration.new)
       end
     end
 
     describe "competitions" do
       it "can create them" do
-        @ability.should be_able_to(:create, Competition.new)
+        should be_able_to(:create, Competition.new)
       end
 
       it "can only update those he created" do
-        @ability.should be_able_to(:update, @competition)
-        @ability.should_not be_able_to(:update, Competition.new)
+        should be_able_to(:update, competition)
+        should_not be_able_to(:update, Competition.new)
       end
     end
 
     describe "schedules" do
       it "can only create schedules for competitions he created" do
-        @ability.should be_able_to(:create, @competition.schedules.build)
-        @ability.should_not be_able_to(:create, Schedule.new)
+        should be_able_to(:create, competition.schedules.build)
+        should_not be_able_to(:create, Schedule.new)
       end
 
       it "can only update schedules for competitions he created" do
-        schedule = create(:schedule, :competition => @competition)
-        @ability.should be_able_to(:update, schedule)
-        @ability.should_not be_able_to(:update, Schedule.new)
+        schedule = create(:schedule, :competition => competition)
+        should be_able_to(:update, schedule)
+        should_not be_able_to(:update, Schedule.new)
       end
     end
   end
